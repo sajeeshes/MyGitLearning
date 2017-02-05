1a978a1ea9d143040b57fcdb33a071e70a74e9ea
Sajeesh <sajeesh@test.com>
2017-02-05 08:43:33 +0530

    Added new changes

---
+++ GitLearning/TestViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TestViewController.swift
+//  GitLearning
+//
+//  Created by Sajeesh ES on 05/02/17.
+//  Copyright © 2017 Genpact. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
