2990b1bfad090c9fdbeecc2c3914ee0187aa352a
Sajeesh <sajeesh@test.com>
2017-02-05 08:53:16 +0530

    Added test changes

--- GitLearning/ViewController.swift
+++ GitLearning/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //Check added 
+        //Check added
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func  viewWillAppear(animated: Bool) {
+        
+    }
 }
 
